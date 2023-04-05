d1068ccf46deb0986b4628b797157e0b641a7a41
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-04-06 04:51:03 +0630

    Add some testing code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.List;
+
+import database.TasksDao;
+import models.Task;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TasksDao tasksDao = new TasksDao();
+
+        // Adding a new task
+        tasksDao.addNewTask(new Task("New task"));
+        
+        // Updating the existing task
+        // Task updatedTask = new Task(3, "Updated task", true);
+        // tasksDao.updateTask(updatedTask);
+
+        // Deleting a task by id
+        // tasksDao.deleteTaskById(3);
+
+        // Retrieving all the tasks
+        List<Task> allTasks = tasksDao.getAllTasks();
+        allTasks.forEach(task -> {
+            System.out.println(task.getId() + " : "  + task.getName() + " : " + task.isDone());
+        });
     }
 }
