e8016439b724ab3388ddab8ed1b2522e8663e7c6
Paing Pyae Man <paingpyaeman04@gmail.com>
2023-04-05 15:53:26 +0630

    Add simple POJO for task

---
+++ src/models/Task.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Task {
+    private int id;
+    private String name;
+    private boolean isDone;
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+}
